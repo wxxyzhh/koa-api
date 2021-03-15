c33b89fa670b9108f55e34a73b308fb002c8fb04
zh <sensitive@163.com>
2021-03-15 23:15:06 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,6 @@
+#### 基于koa.js实现后台通用接口解决方案
+
+:smiley::smiley::smiley::smiley:
+
+包含controlle，service层，及权限校验、错误捕捉等中间件，开箱即用
+
---
+++ config/sqlConfig.js
@@ -0,0 +1,10 @@
+// sql数据库配置
+const config = {
+  mysql_user: 'root', // 用户名
+  mysql_password: 'xxx', // 密码
+  mysql_database: 'xxx' , // 数据库名
+  mysql_host: 'ip', // ip
+  mysql_port: 3306  // 端口号
+}
+
+module.exports = config
